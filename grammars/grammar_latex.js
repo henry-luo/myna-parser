30a004995831768c62c00fcadc182240f76db930
Henry Luo <6037272+henry-luo@users.noreply.github.com>
2018-03-14 17:09:19 +0800

    Added LaTeX parser.

--- grammars/grammar_latex.js
+++ grammars/grammar_latex.js
@@ -5,21 +5,24 @@ function CreateGrammar(myna) {
     let m = myna;    
 
     let g = new function() {
-        // These are helper rules, they do not create nodes in the parse tree.  
-        this.escapedChar    = m.seq('\\', m.char("#$%^&_{}~\\"));
+		// tex command
+		this.option			= m.advanceUntilPast("]").ast;
+		this.params			= m.seq('[', this.option, m.seq(',', this.option).zeroOrMore, ']').opt;
+		this.argument		= m.seq('{', m.advanceUntilPast("}")).ast; // it seems {} can be nested in argument
+		this.name			= m.letters.ast;
+		this.command		= m.seq(m.not('end{'), this.name, this.params.opt, this.argument.zeroOrMore).ast;
 		
-		this.text			= m.
+        this.escapedChar    = m.seq('\\', m.char("#$%^&_{}~\\"), this.argument.opt).ast;  // escapedChar rule not very solid
+		this.comment		= m.seq('%', m.advanceUntilPast(m.newLine), m.char(" \t").zeroOrMore);
+		this.text			= m.notChar("#$%^&_{}~\\").oneOrMore.ast;
 		
-		this.option			= m.advanceUntilPast("]");
-		this.commandParams	= m.seq('[', this.option, ',', this.option, ']');
-		this.argument		= m.advanceUntilPast("}");
-		this.commandArgs	= m.seq('{', this.argument, '}')
-		this.command		= m.seq('\', m.letters, this.commandParams.opt, this.commandArgs.zeroOrMore).ast;
+		// high-level structures
+		let _this = this;
+		this.group			= m.seq('{', m.delay(function() { return _this.latex; }), '}').ast;
+		this.environment	= m.seq('begin', this.argument, m.delay(function() { return _this.latex; }), '\\end', this.argument).ast;
+		this.escaped		= m.seq('\\', m.choice(this.environment, this.command, this.escapedChar));
 		
-		
-       
-		
-        this.latex         = m.braced(m.delimited(this.pair.ws, this.comma)).ast;
+        this.latex			= m.choice(this.escaped, this.group, this.comment, this.text).zeroOrMore.ast;
     };
 
     // Register the grammar, providing a name and the default parse rule
---
+++ test/test_latex.js
@@ -0,0 +1,53 @@
+const fs = require('fs');
+const myna = require('./../myna');
+const grammar = require('./../grammars/grammar_latex')(myna);
+const Mark = require('mark-js');
+const Template = require('mark-template');
+
+function loadSource(fname) {
+	return fs.readFileSync(fname).toString(); 
+}
+
+function writeFile(name, fdata) {
+	fs.writeFileSync(name, fdata);
+}
+
+let obj_id = 0;
+
+function buildAst(ast) {
+	let name = ast.name;
+	// process children
+	let child = [];
+	for (let n of ast.children) {
+		child.push(buildAst(n));
+	}
+	// terminal nodes
+	if (name === 'name' || name === 'escapedChar' || name == 'text' || name == 'option' || name == 'argument') { 
+		child.push(ast.allText);
+	}
+	obj_id++;
+	return Mark(name, {$id:obj_id}, child.length ? child:null);
+}
+
+// Get the parser 
+let parser = myna.parsers.latex;
+
+// Parse some input and print the AST
+// loadSource('./grammars/abnf.abnf');
+// let input = loadSource('./grammars/abnf.abnf');
+let input = loadSource('./test/input/caption.tex');
+
+try {
+    var ast = parser(input);
+	if (ast == null) {
+		console.log("input does not match grammar");
+	} else {
+		// issue: input might not be fully parsed when the parsing ends
+		let markAst = buildAst(ast);
+		console.log(Mark.stringify(markAst, {space:'  '}));
+	}
+}
+catch(e) {
+    console.log("Error occured:");
+    console.log(e.toString());
+}
